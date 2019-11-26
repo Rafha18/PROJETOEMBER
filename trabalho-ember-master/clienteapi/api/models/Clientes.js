d173afa6564f67539964189e7fb4d5f8162f87fa
Rafael Quadra <rafhaqg@gmail.com>
2019-11-26 14:44:48 -0300

    Projeto Ember

---
+++ trabalho-ember-master/clienteapi/api/models/Clientes.js
@@ -0,0 +1,21 @@
+/**
+ * Clientes.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    nome: String,
+    sobrenome: String,
+    endereco: String,
+    telefone: String,
+    email: String
+
+  },
+
+};
+
